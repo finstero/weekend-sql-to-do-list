8eb03fb989f37181236a86522fc9a2448c8b8a01
Olivia Finster <olivia.finster@gmail.com>
2021-05-29 17:09:18 -0500

    addTask function on client side. added click listeners not yet working.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,32 @@ function onLoad(){
     console.log('hello jquery!');
     $(".dropdown-toggle").dropdown();
     getTasks();
+    
+    //click listeners
+    $('#taskButton').on('click', addTask);
+    $('#displayTasks').on('click', '.deleteTask', deleteTask);
+    $('#displayTasks').on('click', '.markComplete', markComplete);
+}
+
+function addTask(){
+    console.log('add task button clicked');
+    let newTask = {};
+    newTask.task = $('#taskIn').val();
+    newTask.notes = $('#notesIn').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: newTask
+    })
+    .then ( response => {
+        console.log('post response from server', response);
+        getTasks();
+    })
+    .catch( error => {
+        console.log('error in post', error);
+        alert('unable to add task. try again later');
+    });
 }
 
 function getTasks(){
@@ -30,7 +56,7 @@ function renderTasks(tasks) {
                 <td>${task.task}</td>
                 <td>${task.priority}</td>
                 <td>${task.notes}</td>
-                <td><button class="markCompleted" data-id="${task.id}" data-complete="${task.complete}">Mark Completed</button></td>
+                <td><button class="markComplete" data-id="${task.id}" data-complete="${task.complete}">Mark Completed</button></td>
                 <td><button class="deleteTask" data-id="${task.id}">Delete Task</button></td>
             </tr>
         `)
