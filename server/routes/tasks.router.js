34c4a087b5352691380fbdba09db783b1f35f396
Olivia Finster <olivia.finster@gmail.com>
2021-05-29 16:41:17 -0500

    route complete with get server side

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const pool = require('../modules/pool');
+
+
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "tasks" ORDER BY "priority";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch (error => {
+    console.log('error getting into koalas table', error);
+    res.sendStatus(500);
+    });
+});
+
+
+
+
+
+module.exports = router
