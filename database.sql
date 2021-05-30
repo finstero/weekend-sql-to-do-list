94fa29095346c9e58e1f80978bf688f714878378
Olivia Finster <olivia.finster@gmail.com>
2021-05-30 13:00:22 -0500

    merged feature branch into master on last commit. added database.sql info.

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "tasks" (
+	"id" SERIAL PRIMARY KEY,
+	"task" VARCHAR(100),
+	"priority" VARCHAR (15),
+	"notes" VARCHAR(255),
+	"complete" BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO "tasks" 
+	("task", "priority", "notes") 
+VALUES 
+('do dishes', 'medium', 'this is the worst task'),
+('finish coding homework', 'high', 'fairly enjoyable'),
+('go on date with partner', 'high', 'maybe a brewery?')
+;
